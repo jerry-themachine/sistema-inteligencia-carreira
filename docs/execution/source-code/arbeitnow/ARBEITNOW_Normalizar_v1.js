230299d5fd3cca66b1003ddb73316da573e62836
Jerry <83985444+jerry-themachine@users.noreply.github.com>
2026-06-08 19:12:34 -0300

    Publish ARBEITNOW normalization script

---
+++ docs/execution/source-code/arbeitnow/ARBEITNOW_Normalizar_v1.js
@@ -0,0 +1,34 @@
+/**
+
+* ARBEITNOW_Normalizar_v1.js
+*
+* Projeto: SIC - Sistema de Inteligência de Carreira
+* Fonte: Arbeitnow
+*
+* Objetivo:
+* Normalizar os dados coletados da plataforma Arbeitnow para o modelo padrão do SIC.
+*
+* Campos Normalizados:
+* * empresa
+* * cargo
+* * descricao
+* * cidade
+* * pais
+* * modalidade
+* * salario
+* * link
+* * fonte
+* * data_coleta
+*
+* Status:
+* Homologado
+*
+* Sprint:
+* SP-010
+*
+* Responsável:
+* Jerry William do Nascimento
+*
+* Implementação:
+* Jarvis
+  */
