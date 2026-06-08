f00a1a807f8958e2df0edb5a2cd7b1077ac6bb56
Jerry <83985444+jerry-themachine@users.noreply.github.com>
2026-06-08 19:09:29 -0300

    Publish RECRUTEI normalization script

---
+++ docs/execution/source-code/recrutei/RECRUTEI_Normalizar_v1.js
@@ -0,0 +1,34 @@
+/**
+
+* RECRUTEI_Normalizar_v1.js
+*
+* Projeto: SIC - Sistema de Inteligência de Carreira
+* Fonte: Recrutei
+*
+* Objetivo:
+* Normalizar os dados coletados da plataforma Recrutei para o modelo padrão do SIC.
+*
+* Campos Normalizados:
+* * empresa
+* * cargo
+* * descricao
+* * cidade
+* * pais
+* * modalidade
+* * salario
+* * link
+* * fonte
+* * data_coleta
+*
+* Status:
+* Em Evolução
+*
+* Sprint:
+* SP-014
+*
+* Responsável:
+* Jerry William do Nascimento
+*
+* Implementação:
+* Jarvis
+  */
