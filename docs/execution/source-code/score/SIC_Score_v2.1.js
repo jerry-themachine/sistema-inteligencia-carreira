ff2937c07337de5c9a2c36c649baba601da8daba
Jerry <83985444+jerry-themachine@users.noreply.github.com>
2026-06-08 19:08:12 -0300

    Publish SIC Score v2.1 script

---
+++ docs/execution/source-code/score/SIC_Score_v2.1.js
@@ -0,0 +1,34 @@
+/**
+
+* SIC_Score_v2.1.js
+*
+* Projeto: SIC - Sistema de Inteligência de Carreira
+*
+* Objetivo:
+* Calcular aderência das vagas ao perfil profissional definido para o SIC.
+*
+* Componentes Avaliados:
+* * Cargo
+* * Ferramentas
+* * Cloud & Plataforma
+* * Engenharia de Dados
+* * Analytics
+*
+* Status:
+* Homologado
+*
+* Versão:
+* v2.1
+*
+* Sprint:
+* SP-014
+*
+* Relacionados:
+* Score v2.1 + Enriquecimento Analítico v1
+*
+* Responsável:
+* Jerry William do Nascimento
+*
+* Implementação:
+* Jarvis
+  */
