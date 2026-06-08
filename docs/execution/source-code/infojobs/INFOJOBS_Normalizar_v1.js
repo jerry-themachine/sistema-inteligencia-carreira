9e5a0ac2da90a89d57d5ac66222f88edfbd661da
Jerry <83985444+jerry-themachine@users.noreply.github.com>
2026-06-08 19:05:53 -0300

    Publish INFOJOBS ID extraction script

---
+++ docs/execution/source-code/infojobs/INFOJOBS_Normalizar_v1.js
@@ -0,0 +1,27 @@
+/**
+
+* INFOJOBS_Extrair_IDs_v1.js
+*
+* Projeto: SIC - Sistema de Inteligência de Carreira
+* Fonte: InfoJobs
+*
+* Objetivo:
+* Extrair IDs e URLs públicas das vagas retornadas pela consulta inicial.
+*
+* Status:
+* Homologado
+*
+* Sprint:
+* SP-014
+*
+* Relacionados:
+* INC-014
+* CR-001
+* DEP-005
+*
+* Responsável:
+* Jerry William do Nascimento
+*
+* Implementação:
+* Jarvis
+  */
