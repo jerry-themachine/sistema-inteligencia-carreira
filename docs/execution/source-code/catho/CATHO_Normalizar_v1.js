9b5bdfbbd9088f399742a3c26f6fde98192d5fbe
Jerry <83985444+jerry-themachine@users.noreply.github.com>
2026-06-08 19:11:28 -0300

    Publish CATHO normalization script

---
+++ docs/execution/source-code/catho/CATHO_Normalizar_v1.js
@@ -0,0 +1,34 @@
+/**
+
+* CATHO_Normalizar_v1.js
+*
+* Projeto: SIC - Sistema de Inteligência de Carreira
+* Fonte: Catho
+*
+* Objetivo:
+* Normalizar os dados coletados da plataforma Catho para o modelo padrão do SIC.
+*
+* Campos Normalizados:
+* * empresa
+* * cargo
+* * descricao
+* * cidade
+* * pais
+* * modalidade
+* * salario
+* * link
+* * fonte
+* * data_coleta
+*
+* Status:
+* Em Evolução
+*
+* Sprint:
+* SP-014
+*
+* Responsável:
+* Jerry William do Nascimento
+*
+* Implementação:
+* Jarvis
+  */
